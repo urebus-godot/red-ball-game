04eca11c651817b5883d94beb2eb9e2cc3a280ad
Serega <satalovserge@gmail.com>
2026-04-27 18:33:12 +0300

    Making game

--- scenes_scripts/game/objects/creatures/move_component.gd
+++ scenes_scripts/game/objects/creatures/move_component.gd
@@ -4,6 +4,11 @@ class_name MoveComponent extends Node
 @export var jump_force: float = 3000.0
 
 @export var creature: Creature
+@export var floor_raycast: RayCast2D
+
+
+func is_on_floor() -> bool:
+	return floor_raycast.is_colliding()
 
 
 func move_right() -> void:
@@ -14,5 +19,10 @@ func move_left() -> void:
 
 
 func jump() -> void:
-	var impulse = Vector2.UP * jump_force
-	creature.apply_central_impulse(impulse)
+	if is_on_floor():
+		var impulse = Vector2.UP * jump_force
+		creature.apply_central_impulse(impulse)
+
+
+func _physics_process(delta: float) -> void:
+	floor_raycast.position = creature.position
