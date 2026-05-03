340dadd69f83ba9ec13f63e752812d9b780efd0f
Serega <satalovserge@gmail.com>
2026-05-03 13:35:39 +0300

    Trying to update terrain

--- scenes_scripts/game/objects/solid/static/terrain.gd
+++ scenes_scripts/game/objects/solid/static/terrain.gd
@@ -5,6 +5,8 @@ extends StaticBody2D
 @onready var ground_bounds: Path2D = $Path2D
 @onready var ground_line: Line2D = $Line2D
 
+@export var areas_without_grass: Array[Vector2] ## X of Vector is left bound; Y of Vector is right bound of area without grass 
+
 
 func _ready() -> void:
 	var ground_curve: Curve2D = ground_bounds.curve
@@ -19,6 +21,8 @@ func _ready() -> void:
 	var width = (ground_line.width / 2) - 4
 
 	for point in line_points:
-		new_points.append(point + Vector2(0, 0))
+		if point.x < areas_without_grass and point.x > areas_without_grass:
+			pass
+		new_points.append(point + Vector2(0, width))
 
 	ground_line.set_points(new_points)
